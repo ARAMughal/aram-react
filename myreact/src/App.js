abc33692bd7e6b474433aae449cc20a336cecab9
Abdur Rahman Asad <56262186+ARAMughal@users.noreply.github.com>
2021-08-24 17:03:12 +0500

    Update App.js

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ARAM React Website
         </a>
       </header>
     </div>
